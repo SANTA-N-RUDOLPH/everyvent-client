cfb9f60190231c7ddf8291c56f58ff25eaaf29b6
Nam Yooseong <nys8765@naver.com>
2025-10-11 22:54:16 +0900

    feat: tailwindcss 설치 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-amber-50">
+      <h3 className="text-amber-200">프로젝트</h3>
+    </div>
+  );
 }
 
 export default App;
