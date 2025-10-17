032a19c4800e4ffbba5b207fbbe3bbd2efdc025d
bomii1 <202103076@hufs.ac.kr>
2025-10-18 02:06:51 +0900

    feat: 사이드바

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,23 +1,33 @@
-// import { Outlet } from "react-router";
-
 import Sidebar from "./Sidebar";
-import Main from "./Main";
+import { useState } from "react";
+import { Outlet } from "react-router";
 
 export default function Layout() {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
   return (
     <div className="bg-global-gradient font-pretendard h-screen w-full flex items-center justify-center">
-      <div className="bg-white/70 backdrop-blur-lg rounded-2xl w-11/12 h-[90vh]">
-        {/* <nav>탑바</nav>
-        <main>
-          <Outlet />
-        </main>
-        <footer>푸터</footer> */}
+      <div className="bg-white/40 backdrop-blur-lg rounded-2xl w-11/12 h-[90vh]">
         <div className="w-full h-full flex gap-4 p-5">
-          <div className="flex-[1]">
-            <Sidebar />
+          <div
+            className={
+              isOpen
+                ? "flex-[1] min-w-42 transition-all duration-300"
+                : "w-16 flex-none transition-all duration-300"
+            }
+          >
+            <Sidebar isOpen={isOpen} onToggle={() => setIsOpen((o) => !o)} />
           </div>
-          <div className="flex-[4]">
-            <Main />
+          <div
+            className={
+              isOpen
+                ? "flex-[5] transition-all duration-300"
+                : "flex-1 transition-all duration-300"
+            }
+          >
+            <main className="w-full h-full bg-white rounded-2xl">
+              <Outlet />
+            </main>
           </div>
         </div>
       </div>
--- src/components/layout/Main.tsx
+++
@@ -1,5 +0,0 @@
-const Main = () => {
-  return <div className="w-full h-full bg-gray-300 rounded-2xl">메인</div>;
-};
-
-export default Main;
