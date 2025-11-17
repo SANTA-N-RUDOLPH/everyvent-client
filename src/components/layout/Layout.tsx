aeb553d4b76f36ad68d13e5a92144b59a884c52c
Nam Yooseong <nys8765@naver.com>
2025-11-17 17:55:46 +0900

    fix: 레이아웃 컴포넌트는 로그인 후 보여줄 UI 담당하도록 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,46 +2,36 @@ import Sidebar from "./Sidebar";
 import { useState } from "react";
 import { Outlet } from "react-router";
 import Header from "./Header";
-import LoginPage from "@pages/LoginPage";
 
 export default function Layout() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
-  // 수정 필요
-  const [isLogined] = useState(false);
-
   return (
     <div className="bg-global-gradient h-screen w-full flex items-center justify-center">
       <div className="bg-white/40 backdrop-blur-lg rounded-2xl w-11/12 h-[90vh]">
-        {isLogined ? (
-          <div className="w-full h-full flex gap-4 p-5">
-            <div
-              className={
-                isOpen
-                  ? "flex-[1] min-w-42 transition-all duration-300"
-                  : "w-16 flex-none transition-all duration-300"
-              }
-            >
-              <Sidebar isOpen={isOpen} onToggle={() => setIsOpen((o) => !o)} />
-            </div>
-            <div
-              className={
-                isOpen
-                  ? "flex-[5] transition-all duration-300"
-                  : "flex-1 transition-all duration-300"
-              }
-            >
-              <main className="w-full h-full bg-white rounded-2xl">
-                <Header />
-                <Outlet />
-              </main>
-            </div>
+        <div className="w-full h-full flex gap-4 p-5">
+          <div
+            className={
+              isOpen
+                ? "flex-[1] min-w-42 transition-all duration-300"
+                : "w-16 flex-none transition-all duration-300"
+            }
+          >
+            <Sidebar isOpen={isOpen} onToggle={() => setIsOpen((o) => !o)} />
           </div>
-        ) : (
-          <div className="w-full h-full flex justify-center items-center">
-            <LoginPage />
+          <div
+            className={
+              isOpen
+                ? "flex-[5] transition-all duration-300"
+                : "flex-1 transition-all duration-300"
+            }
+          >
+            <main className="w-full h-full bg-white rounded-2xl">
+              <Header />
+              <Outlet />
+            </main>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
