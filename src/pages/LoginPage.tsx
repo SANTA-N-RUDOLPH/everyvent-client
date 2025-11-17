101fbaf4b56898a6feda0e48003875e98eba03f2
Nam Yooseong <nys8765@naver.com>
2025-11-17 17:50:22 +0900

    fix: import 경로 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
-import Logo from "@assets/everyvent-logo-basic.png";
-import Kakao from "@assets/login/kakao-logo.png";
-import Naver from "@assets/login/naver-logo.png";
-import Google from "@assets/login/google-logo.png";
-import LoginButton from "@common/LoginButton";
+import Logo from "@/assets/everyvent-logo-basic.png";
+import Kakao from "@/assets/login/kakao-logo.png";
+import Naver from "@/assets/login/naver-logo.png";
+import Google from "@/assets/login/google-logo.png";
+import LoginButton from "@/components/common/LoginButton";
 
 export default function LoginPage() {
   return (
