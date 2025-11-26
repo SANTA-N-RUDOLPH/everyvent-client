1912e740734fe7eb6119d2999a07f0bb5dc8d20b
Nam Yooseong <nys8765@naver.com>
2025-11-26 15:11:20 +0900

    refactor: 스키마 검증 로직 강화

--- src/components/domain/calendar/create-calendar/schema.ts
+++ src/components/domain/calendar/create-calendar/schema.ts
@@ -1,4 +1,5 @@
 import * as z from "zod";
+import { CATEGORYS, COLORS, VISIBILITIES, YEARS } from "./constants";
 
 const baseSchema = z.object({
   title: z
@@ -9,11 +10,28 @@ const baseSchema = z.object({
     .string()
     .min(5, "달력 설명은 5자 이상 입력해주세요.")
     .max(100, "달력 설명은 100자 이내로 입력해주세요."),
-  category: z.string().min(1, "카테고리를 선택해주세요."),
-  color: z.string(),
-  year: z.string().min(1, "달력의 연도를 설정해주세요."),
-  month: z.string().min(1, "달력의 월을 설정해주세요."),
-  visibility: z.string().min(1, "공개 범위를 선택해주세요."),
+  category: z.enum(
+    CATEGORYS.map((category) => category.value) as [string, ...string[]],
+    {
+      error: "카테고리를 선택해주세요."
+    }
+  ),
+  color: z.enum(COLORS.map((color) => color.value) as [string, ...string[]], {
+    error: "색상을 선택해주세요."
+  }),
+  year: z.enum(YEARS as [string, ...string[]], {
+    error: "달력의 연도를 설정해주세요."
+  }),
+  month: z.enum(
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    { error: "달력의 월을 설정해주세요." }
+  ),
+  visibility: z.enum(
+    VISIBILITIES.map((visibility) => visibility.value) as [string, ...string[]],
+    {
+      error: "공개 범위를 선택해주세요."
+    }
+  ),
   previewStartDate: z.string().optional(),
   previewEndDate: z.string().optional()
 });
