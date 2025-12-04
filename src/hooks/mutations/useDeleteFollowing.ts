1b9066d49e8f7ee31b2eeb1f1d21a56a69812768
Nam Yooseong <nys8765@naver.com>
2025-12-04 18:00:47 +0900

    feat: 팔로잉 제거, 팔로워 제거 mutation hook 구현

---
+++ src/hooks/mutations/useDeleteFollowing.ts
@@ -0,0 +1,25 @@
+import { deleteFollowing } from "@/api/follow";
+import { useAuthStore } from "@/stores/useAuthStore";
+import type { FollowItem } from "@/types/follow";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useDeleteFollowing() {
+  const queryClient = useQueryClient();
+  const { user } = useAuthStore();
+
+  return useMutation({
+    mutationFn: (targetId: number) => deleteFollowing(targetId),
+    onSuccess: (_, variables) => {
+      const targetId = variables;
+      console.log(`id ${targetId}번 팔로우 취소 완료`);
+
+      const queryKey = ["followings", user?.id];
+
+      queryClient.setQueryData<FollowItem[]>(queryKey, (oldData) => {
+        if (!oldData) return [];
+
+        return oldData.filter((item) => item.id !== targetId);
+      });
+    }
+  });
+}
